5e43f330dc03137cd680eab00a7f8d5fe5dc359c
SirSalieri <mihailokoprivica480@gmail.com>
2024-06-02 16:16:14 +0200

    small change of the path

--- src/pages/about.php
+++ src/pages/about.php
@@ -151,7 +151,7 @@ footer {
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link" href="../index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="calMAP.html">Calendar</a>
