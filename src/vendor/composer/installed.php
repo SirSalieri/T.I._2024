13b96fb7a7613bdcc67d261ee2c6893db944cd39
SirSalieri <mihailokoprivica480@gmail.com>
2024-06-13 20:50:17 +0200

    Reedited .env, composer and autoload configuration files and relocated installed elements.

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/ti_2024',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '299a4c8938f81893e9aba8c1bcc8325bdddb2556',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/ti_2024' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '299a4c8938f81893e9aba8c1bcc8325bdddb2556',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
